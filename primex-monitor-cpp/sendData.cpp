9f7ba1cd5ebdfc7a6298cac49b2289d5e406020a
dmrtnv <dimasmart2000@gmail.com>
2025-08-06 18:16:44 +0200

    feat: Add api response to log

--- primex-monitor-cpp/sendData.cpp
+++ primex-monitor-cpp/sendData.cpp
@@ -2,20 +2,28 @@
 #include <iostream>
 #include <curl/curl.h>
 
+// This callback will write response data into a std::string
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+	size_t totalSize = size * nmemb;
+	std::string* response = static_cast<std::string*>(userp);
+	response->append(static_cast<char*>(contents), totalSize);
+	return totalSize;
+}
+
 bool sendData(const std::string& url, const std::string& apiKey, const std::string& systemUUID, const std::string& jsonString) {
-	CURL *curl;
+	CURL* curl;
 	CURLcode res;
 	long httpResponseCode = 0;
+	std::string responseBody;
 
 	curl_global_init(CURL_GLOBAL_DEFAULT);
 	curl = curl_easy_init();
 
 	if (curl) {
-		struct curl_slist *headers = NULL;
+		struct curl_slist* headers = NULL;
 		headers = curl_slist_append(headers, "Content-Type: application/json");
 		std::string cookieHeader = "X-API-KEY=" + apiKey + "; X-SYSTEM-UUID=" + systemUUID;
 
-		// curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
 		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
 		curl_easy_setopt(curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
@@ -25,8 +33,11 @@ bool sendData(const std::string& url, const std::string& apiKey, const std::stri
 		curl_easy_setopt(curl, CURLOPT_COOKIE, cookieHeader.c_str());
 		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
-		res = curl_easy_perform(curl);
+		// Set the write callback and pass responseBody as the buffer
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseBody);
 
+		res = curl_easy_perform(curl);
 		std::cout << std::endl;
 
 		if (res != CURLE_OK) {
@@ -38,6 +49,9 @@ bool sendData(const std::string& url, const std::string& apiKey, const std::stri
 		}
 
 		curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpResponseCode);
+
+		std::cout << "HTTP Status: " << httpResponseCode << std::endl;
+		std::cout << "Server Response:\n" << responseBody << std::endl;
 
 		if (httpResponseCode != 201) {
 			std::cerr << "Server responded with error code: " << httpResponseCode << std::endl;
